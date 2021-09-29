e6ec678459d89b5208563dc83e3b29a33efa756f
Ghost Pi <ghostpi@pi.com>
2021-09-29 14:14:43 -0400

    Add 2 servo movement

--- Servos/Servos.ino
+++ Servos/Servos.ino
@@ -1,11 +1,14 @@
 #include <Servo.h> 
 
-Servo myservo;  // create servo object to control a servo 
+Servo base;  // create servo object to control a servo 
+Servo head;  // create servo object to control a servo 
 // a maximum of eight servo objects can be created 
 
-int pos = 0;    // variable to store the servo position 
-int currentPos = 0;
-int speed = 5;
+int pos1 = 0;    // variable to store the servo position 
+int pos2 = 0;    // variable to store the servo position 
+int currentPos1 = 0;
+int currentPos2 = 0;
+int vel = 1;
 int threshold = 10;
 
 const byte numChars = 32;
@@ -15,53 +18,57 @@ boolean newData = false;
 
 
 void setup()  { 
-	myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
-	Serial.begin(9600)
-	myservo.write(0);
+  base.attach(9);  // attaches the servo on pin 9 to the servo object 
+  head.attach(8);
+  Serial.begin(9600);
+  base.write(0);
+  head.write(0);
 } 
 
 
 void loop() { 
-	recvWithEndMarker();
-	showNewData();
-	while(abs(pos - currentPos) < 10) {
-		currentPos += speed * signum(pos-currentPos);
-		myservo.write(currentPos);
-	}
-	
+  recvWithEndMarker();
+  showNewData();
+  currentPos1 += vel * signum(pos1-currentPos1);
+  currentPos2 += vel * signum(pos2-currentPos2);
+  base.write(currentPos1);
+  head.write(currentPos2);
+
 }
 
 void recvWithEndMarker() {
-    static byte ndx = 0;
-    char endMarker = '\n';
-    char rc;
-    
-    while (Serial.available() > 0 && newData == false) {
-        rc = Serial.read();
+  static byte ndx = 0;
+  char endMarker = '\n';
+  char rc;
 
-        if (rc != endMarker) {
-            receivedChars[ndx] = rc;
-            ndx++;
-            if (ndx >= numChars) {
-                ndx = numChars - 1;
-            }
-        }
-        else {
-            receivedChars[ndx] = '\0'; // terminate the string
-            ndx = 0;
-            newData = true;
-        }
+  while (Serial.available() > 0 && newData == false) {
+    rc = Serial.read();
+    if (rc != endMarker) {
+      receivedChars[ndx] = rc;
+      ndx++;
+      if (ndx >= numChars) {
+        ndx = numChars - 1;
+      }
     }
+    else {
+      receivedChars[ndx] = '\0'; // terminate the string
+      ndx = 0;
+      newData = true;
+    }
+  }
 }
 
 void showNewData() {
-    if (newData == true) {
-		pos = atoi(&receivedChars);
-        newData = false;
-    }
+  if (newData == true) {
+    String in = String(receivedChars);
+    pos1 = in.substring(0, 3).toInt();
+    pos2 = in.substring(4, 7).toInt();
+    newData = false;
+  }
 }
 
 int signum(int num) {
-	if (num >= 0) return 1;
-	return -1;
+  if (num >= 0) return 1;
+  return -1;
 }
+
--- movement.py
+++ movement.py
@@ -2,6 +2,11 @@ import time
 import serial
 
 ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)
+ser.flushInput()
 
 def send_angle(x, y):
-    ser.write(','.join([x,y]))
+    ser.write((f'{x:03d},{y:03d}').encode())
+
+
+if __name__ == "__main__":
+    send_angle(180, 0)
