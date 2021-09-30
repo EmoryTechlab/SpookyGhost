c53e1be85eed461d7f37ecb5169b40e9acd17552
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2021-09-30 16:27:13 -0400

    Add angle math

--- detector.py
+++ detector.py
@@ -1,11 +1,16 @@
 import cv2
 import mediapipe as mp
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+DEBUG = False
 
 class Detector():
-    def __init__(self):
-        self.mp_pose = mp.solutions.pose
-        self.mp_drawing = mp.solutions.drawing_utils
+    def __init__(self, width: int, height: int):
         self.cap = None
+        self.width = width
+        self.height = height
 
     def startCapture(self) -> None:
         # For webcam input:
@@ -14,9 +19,10 @@ class Detector():
     def stopCapture(self) -> None:
         if self.cap is not None:
             self.cap.release()
+        cv2.destroyAllWindows()
 
-    def getFace(self, show: bool):
-        with self.mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    def getPose(self):
+        with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
             if self.cap.isOpened():
                 success, image = self.cap.read()
                 if not success:
@@ -27,12 +33,38 @@ class Detector():
                 # Flip the image horizontally for a later selfie-view display, and convert
                 # the BGR image to RGB.
                 image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+                image = cv2.resize(image, (self.width, self.height))                # Resize image
                 # To improve performance, optionally mark the image as not writeable to
                 # pass by reference.
                 image.flags.writeable = False
                 results = pose.process(image)
+                if DEBUG:
+                    mp_drawing.draw_landmarks(
+                        image,
+                        results.pose_landmarks,
+                        mp_pose.POSE_CONNECTIONS,
+                        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+                if results is not None and results.pose_landmarks is not None:
+                    left = results.pose_landmarks.landmark[8]
+                    right = results.pose_landmarks.landmark[7]
+                    if DEBUG:
+                        cv2.circle(image, (int(left.x * self.width), int(left.y * self.height)), 10, (255, 0, 0))
+                        cv2.circle(image, (int(right.x * self.width), int(right.y * self.height)), 10, (255, 0, 0))
+                    return True, ((left.x + right.x)/2, (left.y + right.y)/ 2)
 
-                if results:
-                    return True, results
-                return False
+                if DEBUG:
+                    cv2.imshow('MediaPipe Pose', image)
 
+                return True, None
+
+if __name__ == "__main__":
+    detector = Detector()
+    detector.startCapture()
+    while True:
+        ok, results = detector.getPose()
+        # if results.pose_landmarks is not None:
+            # print(results.pose_landmarks.landmark[12], results.pose_landmarks.landmark[11])
+            # break
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+    detector.stopCapture()
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import detector
 import movement
+import math
+
+FOV = 160
+WIDTH = 400
+HEIGHT = 400
+CENTER = (200, 200)
 
 if __name__ == "__main__":
-    detector = detector.Detector()
+    detector = detector.Detector(WIDTH, HEIGHT)
     detector.startCapture()
-    for i in range(100000):
-        send_angle(90)
-        detected, pose = detector.getFace(True)
+    movement.send_angle(90, 90)
+    while True:
+        detected, pose = detector.getPose()
         if detected:
-            print(pose)
-            
+            angle_base = ((pose[0] - CENTER[0]) * FOV) / WIDTH
+            angle_height = ((pose[1] - CENTER[1]) * FOV) / HEIGHT
+            movement.send_angle(angle_base, angle_height)
+
     detector.stopCapture()
