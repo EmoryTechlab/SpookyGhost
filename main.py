c61e52bc6d9d090e59341f138b828e02b55dee37
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2021-09-30 17:16:45 -0400

    Fix angles

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import detector
 import movement
 import math
+import time
 
 FOV = 160
 WIDTH = 400
 HEIGHT = 400
-CENTER = (200, 200)
+CENTER = (0, 200)
 
 if __name__ == "__main__":
     detector = detector.Detector(WIDTH, HEIGHT)
     detector.startCapture()
-    movement.send_angle(90, 90)
+    movement.send_angle(90, 0)
+    time.sleep(5)
+    print("Tracking...")
     while True:
         detected, pose = detector.getPose()
-        if detected:
-            angle_base = ((pose[0] - CENTER[0]) * FOV) / WIDTH
-            angle_height = ((pose[1] - CENTER[1]) * FOV) / HEIGHT
-            movement.send_angle(angle_base, angle_height)
+        if detected and pose is not None:
+            angle_base = int(((pose[0] * WIDTH - CENTER[0]) * FOV) / WIDTH)
+            angle_height = int(((pose[1] * HEIGHT - CENTER[1]) * FOV) / HEIGHT)
+            movement.send_angle(angle_base, 0)
+            time.sleep(2)
 
     detector.stopCapture()
--- movement.py
+++ movement.py
@@ -1,12 +1,16 @@
 import time
 import serial
 
-ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)
+# ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)
+print("Serial setup...")
+ser = serial.Serial(port="/dev/cu.usbserial-10", baudrate=9600)
 ser.flushInput()
 
 def send_angle(x, y):
-    ser.write((f'{x:03d},{y:03d}').encode())
+    cmd = f'{x:03d},{y:03d}\n'
+    print("Writing: " + cmd)
+    ser.write(cmd.encode())
 
 
 if __name__ == "__main__":
-    send_angle(180, 0)
+    send_angle(90, 0)
